d17fb545a0886678cbace26b863992dbc1a95eb1
Enjifeto <enjifetoabdella01@gmail.com>
2026-05-14 07:51:38 -0700

    my updates

--- schema.sql
+++ schema.sql
@@ -3,11 +3,11 @@ DROP TABLE IF EXISTS Transactions;
 DROP TABLE IF EXISTS Loan;
 DROP TABLE IF EXISTS Employee;
 GO
-CREATE TABLE Employee(
+CREATE TABLE Employee(EmployeeID, FullName, Position, Email, Phone, Salary, BranchID
     employee_id VARCHAR(15) PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
+    full_name VARCHAR(50) NOT NULL,
     position VARCHAR(50),
+    email
     salary DECIMAL(15,2),
     hire_date DATE,
     branch_id VARCHAR(10),
@@ -18,14 +18,10 @@ GO
 CREATE TABLE Loan(
     loan_id VARCHAR(15) PRIMARY KEY,
     customer_id VARCHAR(15),
-    branch_id VARCHAR(10),
     loan_type VARCHAR(30),
     loan_amount DECIMAL(15,2),
-    interest_rate DECIMAL(5,2),
-    duration_in_months INT,
-    start_date DATE,
-    status VARCHAR(20),
-
+    ApplicationDate DATA,
+    branch_id VARCHAR(10),
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
     FOREIGN KEY (branch_id) REFERENCES Branch(branch_id)
 );
@@ -34,12 +30,11 @@ GO
 CREATE TABLE Transactions(
     transaction_id VARCHAR(20) PRIMARY KEY,
     account_no VARCHAR(20),
+    branch_id VARCHAR(10),
     transaction_type VARCHAR(20),
     amount DECIMAL(15,2),
     transaction_date DATETIME,
-    description VARCHAR(200),
-    reference_number VARCHAR(30),
-
+    FOREIGN KEY (branch_id) REFERENCES Branch(branch_id)
     FOREIGN KEY (account_no) REFERENCES Account(account_no)
 );
 GO
